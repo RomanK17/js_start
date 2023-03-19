9b1514c2d2400ab8643ad0842134f83a8f935136
Roman Kopylov <romankopilov1712@gmail.com>
2023-03-19 14:51:14 +0300

    add if operators

--- index.js
+++ index.js
@@ -9,11 +9,25 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-
+if (personalMovieDB.count < 10) {
+    alert("You've seen too few films");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert("You're avarage viewer");
+} else if (personalMovieDB.count > 30) {
+    alert("You're film fun");
+} else {
+    alert("error");
+}
 for (let q = 0; q < 2; q++) {
     let a = prompt("Last watched film?", ""),
         b = prompt("How would you rate it?", "");
-    personalMovieDB.movies[a] = b;
+    if (a == "" || b == "" || a == null || b == null || a.length > 50 || b.length > 50) {
+        alert("error");
+        --q;
+    }
+    else {
+        personalMovieDB.movies[a] = b;
+    }
+
 }
 
-console.log(personalMovieDB);
